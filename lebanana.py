7c027ae612ab4541d01cbbcfdb3001ce758eb4f8
NickT <147011013+NickTGithub@users.noreply.github.com>
2026-02-21 23:38:03 -0800

    Create lebanana.py

---
+++ lebanana.py
@@ -0,0 +1,39 @@
+import board
+import busio
+import displayio
+from fourwire import FourWire
+import time
+import digitalio
+import adafruit_gc9a01a
+
+time.sleep(1)
+
+displayio.release_displays()
+
+vib = digitalio.DigitalInOut(board.D0)
+vib.direction = digitalio.Direction.OUTPUT
+vib.value = True
+
+spi = board.SPI()
+
+display_bus = FourWire(spi,command=board.D6,chip_select=board.D7,reset=board.D2,)
+
+display = adafruit_gc9a01a.GC9A01A(display_bus,width=240,height=240,rotation=0)
+
+bg = displayio.Bitmap(240, 240, 1)
+palette = displayio.Palette(1)
+palette[0] = 0xAA00AA
+
+bitmap = displayio.OnDiskBitmap("/lebanana.bmp")
+
+tile_grid = displayio.TileGrid(bitmap, pixel_shader=bitmap.pixel_shader)
+
+group = displayio.Group()
+group.append(tile_grid)
+
+print('show')
+vib.value = False
+display.root_group = group
+
+while True:
+    pass
