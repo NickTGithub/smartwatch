1ce29fac643a7e06db6523de529a47f4b31d2d54
NickT <147011013+NickTGithub@users.noreply.github.com>
2026-02-22 08:27:48 -0800

    Update lebanana.py

--- lebanana.py
+++ lebanana.py
@@ -20,7 +20,7 @@ display_bus = FourWire(spi,command=board.D6,chip_select=board.D7,reset=board.D2,
 
 display = adafruit_gc9a01a.GC9A01A(display_bus,width=240,height=240,rotation=0)
 
-bitmap = displayio.OnDiskBitmap("/lebanana.bmp")
+bitmap = displayio.OnDiskBitmap("/lebanana.bmp") #put file path in parenthesis. Ex. image.bmp -->"/image.bmp"
 
 tile_grid = displayio.TileGrid(bitmap, pixel_shader=bitmap.pixel_shader)
 
